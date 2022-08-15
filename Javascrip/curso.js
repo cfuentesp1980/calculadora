e1324518436b3b22538929511cb6a5ce825656b1
Claudio Fuentes <claudiofuentesperez@gmail.com>
2022-08-14 23:18:48 -0700

    Siguiendo con el curso

--- Javascrip/curso.js
+++ Javascrip/curso.js
@@ -2,7 +2,7 @@ const h1 = document.querySelector('h1');
 const p = document.querySelector('p');
 const parrafito = document.querySelector('.parrafito');
 const pid = document.querySelector('#pid');
-const input = document.querySelector('#input');
+const input = document.querySelector('input');
 
 
 
@@ -13,3 +13,14 @@ console.log({
     pid,
     input,
 });
+
+/*h1.innerText = 'Patito <br> Feo';
+h1.innerHTML = 'Patito <br> Feo';*/
+
+const img = document.createElement('img');
+img.setAttribute('src','https://static.platzi.com/static/images/conf/logo_black@2x.png');
+console.log(img);
+
+pid.append(img);
+
+console.log(input.value);
